650095a4109d88be11c72f9adde8ab9588460685
hbking00 <harshkumar10522@gmail.com>
2025-07-01 15:37:44 +0530

    test_backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import {asyncHandler} from "../utis/asyncHandler.js"
 
 const registerUser= asyncHandler(async (req , res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+    //get user details from frontend
+    //validation - not empty and many more
+    // check if it is allready exist : usname and email
+    // check image , check avtar
+    //upload them to cloudnary , avatar
+    //create a object - create entry in db
+    //remove pass and refresh token from response
+    //check for user creation 
+    //return res
+
+    const {fullname , email , userName , password}=req.body
+    console.log("email",email);    
 })
 
 
